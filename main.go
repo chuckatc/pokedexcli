7d782de6ca868f2a8f1e83163ac03ffec6bdf9f9
Charles Thornhill-Cole <charlestc@gmail.com>
2025-05-13 12:23:02 +1000

    Add simple REPL

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	for {
+		fmt.Print("Pokedex > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+		input := scanner.Text()
+
+		words := cleanInput(input)
+		command := words[0]
+
+		fmt.Println("Your command was:", command)
+	}
 }
 
 func cleanInput(text string) []string {
